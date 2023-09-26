b7d074e175f3edd60b3776d3f3ef95afe7fcc85e
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-09-26 09:20:51 -0600

    Complete iteration 1

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe Auction do
       @auction.add_item(@item2)
       expect(@auction.items).to eq([@item1, @item2])
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
-
     end
   end
 end
